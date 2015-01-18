a705afd2fe01293bee81c63d1b9c09f5018a94dd
Shawntaulicus <shawnlachance.development@gmail.com>
2015-01-17 21:54:36 -0500

    Initial commit

--- horror-writers/public/js/app.js
+++ horror-writers/public/js/app.js
@@ -2,11 +2,18 @@
 
 
 requirejs.config({
-    paths: {}
+ //   paths: {}
+ shim : {
+        "bootstrap" : { "deps" :['jquery'] }
+    },
+    paths: {
+        "jquery" : "//code.jquery.com/jquery-2.1.1.min",
+        "bootstrap" :  "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min"  
+    }
 });
 
 
-require([/* Dependencies */], function () {
+require([], function () {
 
     var app = {
         initialize: function () {
